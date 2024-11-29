6b4d79cd67c82265e5ac5bdc360c264114e5a484
MohammoD2 <ibr500037@gmail.com>
2024-11-29 08:43:02 +0600

    adding template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='YouTube Comment Analysis involves extracting, processing, and analyzing comments from YouTube videos to uncover insights. This analysis can provide valuable information about audience sentiment, engagement trends, and the effectiveness of content strategies.YouTube Comment Analysis involves extracting, processing, and analyzing comments from YouTube videos to uncover insights. This analysis can provide valuable information about audience sentiment, engagement trends, and the effectiveness of content strategies.',
+    author='Mohammod Ibrahim Hossain',
+    license='MIT',
+)
